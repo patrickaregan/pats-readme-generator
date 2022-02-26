843dcb74f8fe041b1840563617beaf9042cbbe2f
patrickaregan <patrickaregan@gmail.com>
2022-02-26 17:24:15 -0600

    populate questions array with some of the questions

--- index.js
+++ index.js
@@ -2,22 +2,41 @@
 const inquirer = require("inquirer");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "projectTitle",
+        message: "What is the title of your project?"
+    },
+    {
+        type: "input",
+        name: "descMotivation",
+        message: "What was your motivation for the project?"
+    },
+    {
+        type: "input",
+        name: "descWhyBuild",
+        message: "Why did you build the project?"
+    },
+    {
+        type: "input",
+        name: "descWhatProblemSolved",
+        message: "What problem does the project solve?"
+    },
+    {
+        type: "input",
+        name: "descWhatDidYouLearn",
+        message: "What did you learn from building the project?"
+    },
+];
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
 function init() {
-    inquirer
-        .prompt([
-            {
-                type: "input",
-                name: "firstName",
-                message: "What is your first name?"
-            }
-        ])
-        .then(answers => console.log(answers));
+    inquirer.prompt(questions).then(answers => console.log(answers));
 }
 
 // Function call to initialize app
